de61184709112b20ef6bce882c2a08c943887d57
adella <scratchglory@gmail.com>
2020-10-26 20:39:31 -0400

    removed password in reg form

--- src/components/Forms/Register.js
+++ src/components/Forms/Register.js
@@ -5,24 +5,51 @@ class Register extends Component {
     super(props);
     this.state = {
       fName: '',
+      lName: '',
+      email: '',
+      phone: '',
+      zipcode: '',
+      password: '',
     };
   }
 
   handleSubmit = event => {
     event.preventDefault();
+    //   testing the handlesubmit
+    const data = this.state;
+    console.log('Final Data', data);
   };
 
   handleInputChange = event => {
     event.preventDefault();
-    console.log('handleINputChange Name', event.target.name);
-    console.log('handleINputChange Value', event.target.value);
+    //   getting name of input and value
+    // console.log('handleINputChange Name', event.target.name);
+    //   console.log('handleINputChange Value', event.target.value);
+    this.setState({
+      // seting key to key-value pair
+      [event.target.name]: event.target.value,
+    });
   };
 
+  componentDidMount() {
+    this.setState({
+      fName: this.state.fName,
+      lName: this.state.lName,
+      email: this.state.email,
+      phone: this.state.phone,
+      zipcode: this.state.zip,
+      password: this.state.password,
+    });
+  }
+
   render() {
+    //   change state based on whats coming in input
     const { fName } = this.state;
+
     return (
       <div>
         <h1>User Registration</h1>
+        {/* To test functionality */}
         <p>First name is: {fName}</p>
         <form onSubmit={this.handleSubmit}>
           <p>
@@ -37,6 +64,7 @@ class Register extends Component {
             <input
               type="text"
               placeholder="Last Name"
+              name="lName"
               onChange={this.handleInputChange}
             />
           </p>
@@ -44,18 +72,34 @@ class Register extends Component {
             <input
               type="email"
               placeholder="email"
-              value={this.state.email}
+              name="email"
               onChange={this.handleInputChange}
             />
           </p>
           <p>
             <input
-              type="password"
-              placeholder="Password"
-              // value={this.state.password}
+              type="text"
+              placeholder="phone #"
+              name="phone"
               onChange={this.handleInputChange}
             />
           </p>
+          <p>
+            <input
+              type="text"
+              placeholder="zipcode"
+              name="zipcode"
+              onChange={this.handleInputChange}
+            />
+          </p>
+          {/* <p>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              onChange={this.handleInputChange}
+            />
+          </p> */}
           <p>
             <label>Are you a: </label>
             <select onChange={this.handleInputChange} defaultValue="Select">
