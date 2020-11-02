26c003946140e741b0f0bb9390bd7e8f8924421d
William Schwindt <williammschwindt@gmail.com>
2020-11-01 17:09:04 -0800

    added antd template

--- src/components/pages/GroomerDashboard/RenderGroomerDashboard.js
+++ src/components/pages/GroomerDashboard/RenderGroomerDashboard.js
@@ -1,5 +1,56 @@
 import React from 'react';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import {
+  DesktopOutlined,
+  PieChartOutlined,
+  FileOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+
+const { Header, Content, Footer, Sider } = Layout;
 
 export const RenderGroomerDashboard = props => {
-  return <div></div>;
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider
+        collapsible
+        collapsed={props.collapsed}
+        onCollapse={props.onCollapse}
+      >
+        <div className="logo" />
+        <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline">
+          <Menu.Item key="1" icon={<PieChartOutlined />}>
+            Home
+          </Menu.Item>
+          <Menu.Item key="2" icon={<DesktopOutlined />}>
+            Profile
+          </Menu.Item>
+          <Menu.Item key="3" icon={<UserOutlined />}>
+            Appointments
+          </Menu.Item>
+          <Menu.Item key="5" icon={<FileOutlined />}>
+            Logout
+          </Menu.Item>
+        </Menu>
+      </Sider>
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+        <Content style={{ margin: '0 16px' }}>
+          <Breadcrumb style={{ margin: '16px 0' }}>
+            <Breadcrumb.Item>User</Breadcrumb.Item>
+            <Breadcrumb.Item>Bill</Breadcrumb.Item>
+          </Breadcrumb>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            Profile picture and calendar displayed here
+          </div>
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>
+          Ant Design ©2018 Created by Ant UED
+        </Footer>
+      </Layout>
+    </Layout>
+  );
 };
