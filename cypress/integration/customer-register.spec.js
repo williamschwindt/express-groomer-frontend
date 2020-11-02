33db64e304b089f5d37e6c4f48a77f71b77e9868
Luis Abellan <luis.abellan.dev@gmail.com>
2020-11-02 09:42:45 +0100

    all tests passing

--- cypress/integration/customer-register.spec.js
+++ cypress/integration/customer-register.spec.js
@@ -33,15 +33,17 @@ describe('Login page tests', function(){
     
 
     it('Customer can register', function () {
-        cy.get('input[name="fName"]').type('Mary');
-        cy.get('input[name="lName"]').type('Cinderella');
+        cy.get('input[name="name"]').type('Mary');
+        cy.get('input[name="lastname"]').type('Cinderella');
         cy.get('input[name="email"]').type('mary@cinderella.com');
         cy.get('input[name="phone"]').type('969497865');
         cy.get('input[name="address"]').type("23 Ann's Rd");
         cy.get('input[name="city"]').type('Atlanta');
         cy.get('input[name="state"]').type('Georgia');
         cy.get('input[name="zipcode"]').type('846891');
-        cy.get('input[placeholder="Talk about yourself!"]').type('I love dogs!');
+        cy.get('input[name="description"]').type('I love dogs!');
+        cy.get('input[name="photoUrl"]').type('https://flikr.com/myphoto.jpg');
+
 
         // If we click on "Register" returns error for now, it will clear once we create postData function in Register-Customer.js
         // if we finally implement registration.
