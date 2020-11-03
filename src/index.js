581f87f6a501fc5de05dee37113c3b795c641f07
William Schwindt <williammschwindt@gmail.com>
2020-11-02 17:18:00 -0800

    moved actions and reducers into state folder

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
-import { rootReducer } from './reducers/rootReducer';
+import { rootReducer } from './state/reducers/rootReducer';
 import {
   BrowserRouter as Router,
   Route,
