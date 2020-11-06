9dfb64d65baff730418f95a9bc8e0d506531bc89
luisabellan <luis.abellan.dev@gmail.com>
2020-11-06 15:00:49 +0100

    save

--- src/__tests__/FormInput.test.js
+++ src/__tests__/FormInput.test.js
@@ -10,9 +10,16 @@ describe('<FormInput /> test suite', () => {
   });
   test('input is rendered with proper labels', () => {
     // For this assertion, we're just using RTL to test the attributes that get rendered from FormInput
-    const { getByLabelText } = render(<FormInput label="User Email" />);
+    const { getByLabelText } = render(
+      <FormInput
+        labelId="User Email"
+        name="username"
+        placeholder="User Email"
+      />
+    );
 
     const input = getByLabelText(/user email/i);
+    expect(input.type).toBe('text');
     expect(input.placeholder).toBe('User Email');
   });
 });
