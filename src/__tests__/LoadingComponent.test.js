569c8bcf396c8c84c05ef493a9000299d97fe4a0
luisabellan <luis.abellan.dev@gmail.com>
2020-11-06 12:22:25 +0100

    this test didn't pass after Cory modified HomeContainer.js commit: a8eee0427675b586367ea89147fa2b7e25141c5d, it's fixed now

--- src/__tests__/LoadingComponent.test.js
+++ src/__tests__/LoadingComponent.test.js
@@ -6,10 +6,14 @@ import { LoadingComponent } from '../components/common';
 describe('Loading Common Component', () => {
   test('it should mount a div based on props', () => {
     const { getByText, rerender } = render(
-      <LoadingComponent message="Loading Content" />
+      <LoadingComponent message="you need to register before you can view dashboard" />
     );
-    const message = getByText(/loading content/i);
-    expect(message.textContent).toBe('Loading Content');
+    const message = getByText(
+      /you need to register before you can view dashboard/i
+    );
+    expect(message.textContent).toBe(
+      'you need to register before you can view dashboard'
+    );
     rerender(<LoadingComponent message="Loading User Profile" />);
     const newMessage = getByText(/loading user profile/i);
     expect(newMessage.textContent).toBe('Loading User Profile');
