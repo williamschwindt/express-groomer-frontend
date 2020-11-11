21850ab1fd46e15658237a3115613fb390417281
Luis Abellan <luis.abellan.dev@gmail.com>
2020-11-11 14:54:17 +0100

    Added google map component to Groomer Profile
    * Added google map component to Groomer Profile
    * Installed react-geocode to find latitude and longitude for a given address (to be implemented later on)
    * corrected routes after env variables have been modified in our back-end

--- src/components/pages/GroomerProfile/RenderGroomerProfile.js
+++ src/components/pages/GroomerProfile/RenderGroomerProfile.js
@@ -3,6 +3,10 @@ import { Row, Col, Avatar, Modal, Button, Breadcrumb, Form, Input } from 'antd';
 import { UserOutlined } from '@ant-design/icons';
 import MyMap from '../../../components/MyMap/MyMap';
 
+const style = {
+  minHeight: '300px',
+};
+
 const DemoBox = props => (
   <div className={`height-${props.value}`}>{props.children}</div>
 );
@@ -115,12 +119,16 @@ export const RenderGroomerProfile = props => {
         <Col xs={20} sm={16} md={12} lg={8} xl={4} />
         <Col xs={2} sm={4} md={6} lg={8} xl={10}>
           <DemoBox value={50}>Calendar Here</DemoBox>
-          <DemoBox value={50}>
+          <div id="map">
             <MyMap
-              latitude={props.dummyData.latitude}
-              longitude={props.dummyData.longitude}
+              style={style}
+              zoom={10}
+              initialCenter={{
+                lat: -1.2884,
+                lng: 36.8233,
+              }}
             />
-          </DemoBox>
+          </div>
         </Col>
       </Row>
     </>
