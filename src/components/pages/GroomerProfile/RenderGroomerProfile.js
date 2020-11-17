e5ac264bed831ef3304a14251bfbdb35d3af9f44
Luis Abellan <luis.abellan.dev@gmail.com>
2020-11-17 03:59:11 +0100

    deleted comments

--- src/components/pages/GroomerProfile/RenderGroomerProfile.js
+++ src/components/pages/GroomerProfile/RenderGroomerProfile.js
@@ -131,8 +131,8 @@ export const RenderGroomerProfile = props => {
               // }}
               zoom={10}
               initialCenter={{
-                lat: props.dummyData.latitude, // -1.2884
-                lng: props.dummyData.longitude, // 36.8233
+                lat: props.dummyData.latitude,
+                lng: props.dummyData.longitude,
               }}
             />
           </div>
