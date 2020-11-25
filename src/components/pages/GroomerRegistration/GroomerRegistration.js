a001938a03139a3c218b71d89bdaa261c83c00d9
William Schwindt <williammschwindt@gmail.com>
2020-11-24 17:56:38 -0800

    groomer registration styled

--- src/components/pages/GroomerRegistration/GroomerRegistration.js
+++ src/components/pages/GroomerRegistration/GroomerRegistration.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
 import { registerGroomer } from '../../../api/index';
 
 const GroomerRegistration = props => {
   const defaultUser = {
     name: '',
     lastname: '',
-    email: props.location.state.email,
+    email: props.email,
     phone: '',
     zip: '',
     address: '',
@@ -29,36 +30,27 @@ const GroomerRegistration = props => {
 
   const handleInputChange = event => {
     event.preventDefault();
-    //   getting name of input and value
-
     setUser({
       ...user,
-      // seting key to key-value pair
       [event.target.name]: event.target.value,
     });
   };
 
   return (
-    //   change user.state based on whats coming in input
-
     <div className="registration-container">
-      <h1>Groomer Registration</h1>
-
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="name">Name: </label>
+        <h1>Become a Groomer</h1>
+        <label htmlFor="name">Name </label>
 
-        {/* use aria-invalid to indicate field contain error */}
         <input
           type="text"
           id="name"
           name="name"
-          placeholder="First Name"
           onChange={handleInputChange}
           aria-invalid={errors.name ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.name && errors.name.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -66,18 +58,16 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="lastname">Last Name: </label>
+        <label htmlFor="lastname">Last Name </label>
         <input
           type="text"
           id="lastname"
           name="lastname"
-          placeholder="Last Name"
           onChange={handleInputChange}
           aria-invalid={errors.lastname ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.lastname && errors.lastname.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -85,18 +75,17 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="phone">Phone: </label>
+        <label htmlFor="phone">Phone </label>
+
         <input
           type="text"
           id="phone"
           name="phone"
-          placeholder="Phone"
           onChange={handleInputChange}
           aria-invalid={errors.phone ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.phone && errors.phone.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -104,18 +93,17 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="address">Address: </label>
+        <label htmlFor="address">Address </label>
+
         <input
           type="text"
           id="address"
           name="address"
-          placeholder="Address"
           onChange={handleInputChange}
           aria-invalid={errors.address ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.address && errors.address.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -123,18 +111,17 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="city">City: </label>
+        <label htmlFor="city">City </label>
+
         <input
           type="text"
           id="city"
           name="city"
-          placeholder="City"
           onChange={handleInputChange}
           aria-invalid={errors.city ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.city && errors.city.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -142,18 +129,17 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="state">State: </label>
+        <label htmlFor="state">State </label>
+
         <input
           type="text"
           id="state"
           name="state"
-          placeholder="State"
           onChange={handleInputChange}
           aria-invalid={errors.state ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.state && errors.state.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -161,18 +147,16 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="country">Country: </label>
+        <label htmlFor="country">Country </label>
         <input
           type="text"
           id="country"
           name="country"
-          placeholder="Country"
           onChange={handleInputChange}
           aria-invalid={errors.country ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.country && errors.country.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -180,18 +164,17 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="zipcode">ZIP Code: </label>
+        <label htmlFor="zipcode">Zipcode </label>
+
         <input
           type="text"
           id="zipcode"
           name="zip"
-          placeholder="zipcode"
           onChange={handleInputChange}
           aria-invalid={errors.zipcode ? 'true' : 'false'}
           ref={register({ required: true, maxLength: 30 })}
         />
 
-        {/* use role="alert" to announce the error message */}
         {errors.zipcode && errors.zipcode.type === 'required' && (
           <span role="alert">This is required</span>
         )}
@@ -199,100 +182,10 @@ const GroomerRegistration = props => {
           <span role="alert">Max length exceeded</span>
         )}
 
-        <label htmlFor="description">Description: </label>
-        <input
-          type="text"
-          id="description"
-          name="description"
-          placeholder="description"
-          onChange={handleInputChange}
-          aria-invalid={errors.description ? 'true' : 'false'}
-          ref={register({ required: true, maxLength: 300 })}
-        />
-
-        {/* use role="alert" to announce the error message */}
-        {errors.description && errors.description.type === 'required' && (
-          <span role="alert">This is required</span>
-        )}
-        {errors.description && errors.description.type === 'maxLength' && (
-          <span role="alert">Max length exceeded</span>
-        )}
-
-        <label htmlFor="photoUrl">Photo URL: </label>
-        <input
-          type="text"
-          id="photoUrl"
-          name="photo_url"
-          placeholder="Photo URL"
-          onChange={handleInputChange}
-          aria-invalid={errors.zipcode ? 'true' : 'false'}
-          ref={register({ required: false, maxLength: 300 })}
-        />
-
-        {/* use role="alert" to announce the error message */}
-        {errors.photoUrl && errors.photoUrl.type === 'maxLength' && (
-          <span role="alert">Max length exceeded</span>
-        )}
-
-        <label htmlFor="walk_rate">Walk rate: </label>
-        <input
-          type="text"
-          id="walk_rate"
-          name="walk_rate"
-          placeholder="Walk rate"
-          onChange={handleInputChange}
-          aria-invalid={errors.walk_rate ? 'true' : 'false'}
-          ref={register({ required: true, maxLength: 100 })}
-        />
-
-        {/* use role="alert" to announce the error message */}
-        {errors.walk_rate && errors.walk_rate.type === 'required' && (
-          <span role="alert">This is required</span>
-        )}
-        {errors.walk_rate && errors.walk_rate.type === 'maxLength' && (
-          <span role="alert">Max length exceeded</span>
-        )}
-
-        <label htmlFor="day_care_rate">Day care rate: </label>
-        <input
-          type="text"
-          id="day_care_rate"
-          name="day_care_rate"
-          placeholder="Day care rate"
-          onChange={handleInputChange}
-          aria-invalid={errors.day_care_rate ? 'true' : 'false'}
-          ref={register({ required: true, maxLength: 100 })}
-        />
-
-        {/* use role="alert" to announce the error message */}
-        {errors.day_care_rate && errors.day_care_rate.type === 'required' && (
-          <span role="alert">This is required</span>
-        )}
-        {errors.day_care_rate && errors.day_care_rate.type === 'maxLength' && (
-          <span role="alert">Max length exceeded</span>
-        )}
-        <label htmlFor="vet_visit_rate">Vet visit rate: </label>
-
-        <input
-          type="text"
-          id="vet_visit_rate"
-          name="vet_visit_rate"
-          placeholder="Vet visit rate"
-          onChange={handleInputChange}
-          aria-invalid={errors.vet_visit_rate ? 'true' : 'false'}
-          ref={register({ required: true, maxLength: 100 })}
-        />
-
-        {/* use role="alert" to announce the error message */}
-        {errors.vet_visit_rate && errors.vet_visit_rate.type === 'required' && (
-          <span role="alert">This is required</span>
-        )}
-        {errors.vet_visit_rate &&
-          errors.vet_visit_rate.type === 'maxLength' && (
-            <span role="alert">Max length exceeded</span>
-          )}
-
         <button type="submit">Submit</button>
+        <p>
+          Already have an account? <Link to="/login">Sign in now</Link>
+        </p>
       </form>
     </div>
   );
